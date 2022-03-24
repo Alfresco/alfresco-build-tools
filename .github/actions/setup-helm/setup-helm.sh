f8a16ce596219d35a8bb9c82ca21acacfea95a2f
Giovanni Toraldo <giovanni.toraldo@hyland.com>
2022-03-24 17:30:56 +0100

    external script

---
+++ .github/actions/setup-helm/setup-helm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+HELM_VERSION=$1
+WIN_PATH=${WIN_PATH:-$HOME/bin/}
+LIN_PATH=${LIN_PATH:-/usr/local/bin/}
+if [ "$RUNNER_OS" == "Windows" ]; then
+    curl -fsSLo helm.zip https://get.helm.sh/helm-v${HELM_VERSION}-windows-amd64.zip
+    unzip -o helm.zip windows-amd64/helm.exe && rm helm.zip && mv windows-amd64/helm.exe "${WIN_PATH}" && rmdir windows-amd64
+elif [ "$RUNNER_OS" == "Linux" ]; then
+    curl -fsSL https://get.helm.sh/helm-v${HELM_VERSION}-$(uname | tr '[:upper:]' '[:lower:]')-amd64.tar.gz | tar xz --strip=1 -C "${LIN_PATH}" $(uname | tr '[:upper:]' '[:lower:]')-amd64/helm
+else
+    echo "$RUNNER_OS not supported"
+    exit 1
+fi
+
+echo helm $(helm version --client --short)
--- README.md
+++ README.md
@@ -4,3 +4,19 @@
 [![pre-commit](https://img.shields.io/badge/pre--commit-enabled-brightgreen?logo=pre-commit&logoColor=white)](https://github.com/pre-commit/pre-commit)
 
 Shared [Travis CI](https://travis-ci.com/) and [pre-commit](https://pre-commit.com/) configuration files plus misc tools.
+
+## GitHub Actions
+
+### setup-helm
+
+If you need the helm cli available in the runner path:
+
+```yml
+jobs:
+
+  test:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v1
+      - uses: Alfresco/alfresco-build-tools/.github/actions/setup-helm@master
+```
