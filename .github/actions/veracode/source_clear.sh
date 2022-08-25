caf8eeba5306f5e53519185905f9a2d1941a309e
Damian Ujma <92095156+damianujma@users.noreply.github.com>
2022-08-25 15:19:20 +0200

    ACS-3436 Fix Source Clear script + update README.md (#113)
    * ACS-3436 Fix pushd command
    
    * ACS-3436 Debug logs
    
    * ACS-3436 Debug logs
    
    * ACS-3436 Remove pushd line
    
    * ACS-3436 Remove debug logging
    
    * ACS-3436 Update README.md
    
    * ACS-3436 Remove unnecessary 'popd'

--- .github/actions/veracode/source_clear.sh
+++ .github/actions/veracode/source_clear.sh
@@ -3,7 +3,6 @@
 echo "=========================== Starting SourceClear Script ==========================="
 PS4="\[\e[35m\]+ \[\e[m\]"
 set +e -v -x
-pushd "$(dirname "${BASH_SOURCE[0]}")/../"
 
 mvn -B -q clean install \
     -DskipTests \
@@ -19,7 +18,6 @@ fi
 
 grep -e 'Full Report Details' -e 'Failed' scan.log
 
-popd
 set +vex
 echo "=========================== Finishing SourceClear Script =========================="
 
