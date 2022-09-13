ab5e8e5b5c6b9017c98833f8f7da7bb54d4468e9
Kacper Klassa <kacper.klassa@hyland.com>
2022-09-13 16:47:12 +0200

    Revert "try using full script from aps-deployment"
    This reverts commit dfff7b419f6a43e8e9f2c608ee068826892fed35.

--- .github/actions/dbp-charts/verify-helm/helm_install.sh
+++ .github/actions/dbp-charts/verify-helm/helm_install.sh
@@ -1,5 +1,9 @@
-#!/bin/bash -e
+#!/bin/bash
 
+if [ -z "${ACS_VERSION}" ]; then
+  echo "ACS_VERSION variable is not set"
+  exit 2
+fi
 if [ -z "${COMMIT_MESSAGE}" ]; then
   echo "COMMIT_MESSAGE variable is not set"
   exit 2
@@ -25,11 +29,20 @@ if [ -z "${BRANCH_NAME}" ]; then
   exit 2
 fi
 
+
+TEST_NEWMAN="true"
+if [[ "${ACS_VERSION}" == "default" ]]; then
+  ACS_VERSION="latest"
+  TEST_NEWMAN="false"
+fi
+
 GIT_DIFF="$(git diff origin/master --name-only .)"
-namespace=$(echo "${BRANCH_NAME}" | cut -c1-28 | tr /_ - | tr -d '[:punct:]' | awk '{print tolower($0)}')"-${GITHUB_RUN_NUMBER}"
-release_name_ingress=aps-ing-"${GITHUB_RUN_NUMBER}"
-release_name_aps=aps-"${GITHUB_RUN_NUMBER}"
+VALID_VERSION=$(echo "${ACS_VERSION}" | tr -d '.' | awk '{print tolower($0)}')
+namespace=$(echo "${BRANCH_NAME}" | cut -c1-28 | tr /_ - | tr -d [:punct:] | awk '{print tolower($0)}')-"${GITHUB_RUN_NUMBER}"-"${VALID_VERSION}"
+release_name_ingress=ing-"${GITHUB_RUN_NUMBER}"-"${VALID_VERSION}"
+release_name_acs=acs-"${GITHUB_RUN_NUMBER}"-"${VALID_VERSION}"
 HOST=${namespace}.${DOMAIN}
+PROJECT_NAME=alfresco-content-services
 
 # pod status
 pod_status() {
@@ -99,6 +112,7 @@ newman() {
   done
   return 1
 }
+
 prepare_namespace() {
   cat <<EOF | kubectl apply -f -
 apiVersion: v1
@@ -153,18 +167,21 @@ EOF
 }
 
 export values_file=helm/"${PROJECT_NAME}"/values.yaml
+if [[ ${ACS_VERSION} != "latest" ]]; then
+  values_file="helm/${PROJECT_NAME}/${ACS_VERSION}_values.yaml"
+fi
 
 if [[ "${BRANCH_NAME}" == "master" ]] ||
   [[ "${COMMIT_MESSAGE}" == *"[run all tests]"* ]] ||
   [[ "${COMMIT_MESSAGE}" == *"[release]"* ]] ||
+  [[ "${GIT_DIFF}" == *helm/${PROJECT_NAME}/${ACS_VERSION}_values.yaml* ]] ||
   [[ "${GIT_DIFF}" == *helm/${PROJECT_NAME}/templates* ]] ||
   [[ "${GIT_DIFF}" == *helm/${PROJECT_NAME}/charts* ]] ||
   [[ "${GIT_DIFF}" == *helm/${PROJECT_NAME}/requirements* ]] ||
-  [[ "${GIT_DIFF}" == *${values_file}* ]] ||
+  [[ "${GIT_DIFF}" == *helm/${PROJECT_NAME}/values.yaml* ]] ||
   [[ "${GIT_DIFF}" == *test/postman/helm* ]]; then
   echo "deploying..."
 else
-  echo "::warning ::Skipping tests since we are not on master and no changes has been made to helm folder"
   exit 0
 fi
 
@@ -194,15 +211,15 @@ helm upgrade --install "${release_name_ingress}" --repo https://kubernetes.githu
 
 # install acs
 helm dep up helm/"${PROJECT_NAME}"
-helm upgrade --install "${release_name_aps}" helm/"${PROJECT_NAME}" \
+helm upgrade --install "${release_name_acs}" helm/"${PROJECT_NAME}" \
   --values="${values_file}" \
+  --set global.tracking.sharedsecret="$(openssl rand -hex 24)" \
   --set externalPort="443" \
   --set externalProtocol="https" \
   --set externalHost="${HOST}" \
   --set persistence.enabled=true \
   --set persistence.storageClass.enabled=true \
   --set persistence.storageClass.name="nfs-client" \
-  --set postgresql.persistence.existingClaim="" \
   --set global.alfrescoRegistryPullSecrets=quay-registry-secret \
   --wait \
   --timeout 20m0s \
@@ -232,7 +249,57 @@ done
 
 pods_ready || exit 1
 
+if [[ "${TEST_NEWMAN}" == "true" ]]; then
+
+  # Delay running the tests to give ingress & SOLR a chance to fully initialise
+  echo "Waiting 3 minutes from $(date) before running tests..."
+  sleep 180
+
+  # run acs checks
+  wait_for_connection
+  newman run helm/acs-test-helm-collection.json --global-var "protocol=https" --global-var "url=${HOST}"
+  TEST_RESULT=$?
+  echo "TEST_RESULT=${TEST_RESULT}"
+  if [[ "${TEST_RESULT}" == "0" ]]; then
+    TEST_RESULT=0
+    # run sync service checks
+    if [[ ${ACS_VERSION} != "community" ]]; then
+      wait_for_connection
+      newman run "helm/sync-service-test-helm-collection.json" --global-var "protocol=https" --global-var "url=${HOST}"
+      TEST_RESULT=$?
+      echo "TEST_RESULT=${TEST_RESULT}"
+    fi
+
+    if [[ "${TEST_RESULT}" == "0" ]] && [[ ${ACS_VERSION} == "latest" ]]; then
+      # For checking if persistence failover is correctly working with our deployments
+      # in the next phase we delete the acs and postgresql pods,
+      # wait for k8s to recreate them, then check if the data created in the first test run is still there
+      kubectl delete pod -l app="${release_name_acs}"-alfresco-cs-repository,component=repository -n "${namespace}"
+      kubectl delete pod -l app=postgresql-acs,release="${release_name_acs}" -n "${namespace}"
+      helm upgrade "${release_name_acs}" helm/"${PROJECT_NAME}" \
+        --wait \
+        --timeout 10m0s \
+        --reuse-values \
+        --namespace="${namespace}"
+
+      # check pods
+      pods_ready || exit 1
+
+      # run checks after pod deletion
+      wait_for_connection
+      newman run "helm/acs-validate-volume-collection.json" --global-var "protocol=https" --global-var "url=${HOST}"
+      TEST_RESULT=$?
+      echo "TEST_RESULT=${TEST_RESULT}"
+    fi
+  fi
+fi
+
 if [[ "${COMMIT_MESSAGE}" != *"[keep env]"* ]]; then
-  helm delete "${release_name_ingress}" "${release_name_aps}" -n "${namespace}"
+  helm delete "${release_name_ingress}" "${release_name_acs}" -n "${namespace}"
   kubectl delete namespace "${namespace}" --grace-period=1
+fi
+
+if [[ "${TEST_RESULT}" == "1" ]]; then
+  echo "Tests failed, exiting"
+  exit 1
 fi
