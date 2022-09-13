8dc20e3511cbc47e4574c512745c987451c149aa
Elias Ricken de Medeiros <26007058+erdemedeiros@users.noreply.github.com>
2022-09-13 15:45:19 +0200

    AAE-8916 Add the possiblity to specify the type of the prelease

--- .github/actions/calculate-next-internal-version/next-prerelease.sh
+++ .github/actions/calculate-next-internal-version/next-prerelease.sh
@@ -4,11 +4,10 @@ set -e
 echo "Fetching tags ... "
 git fetch --tags
 
-PRERELEASE_LABEL="alpha"
-FIRST_PRERELEASE_SUFFIX="-${PRERELEASE_LABEL}.1"
+FIRST_PRERELEASE_SUFFIX="-${PRERELEASE_TYPE}.1"
 
 echo "Next version: $NEXT_VERSION"
-LATEST_PRERELEASE="$(git tag --sort=-creatordate | grep -m 1  "^$NEXT_VERSION\-$PRERELEASE_LABEL\.[[:digit:]]\{1,4\}$" | cat)"
+LATEST_PRERELEASE="$(git tag --sort=-creatordate | grep -m 1  "^$NEXT_VERSION\-$PRERELEASE_TYPE\.[[:digit:]]\{1,4\}$" | cat)"
 if [ -n "$LATEST_PRERELEASE" ]; then
   echo "Latest prerelease version found: $LATEST_PRERELEASE"
   NEXT_PRERELEASE="$(pysemver bump prerelease "$LATEST_PRERELEASE")"
