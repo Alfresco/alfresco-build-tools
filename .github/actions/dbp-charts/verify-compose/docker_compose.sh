43169867eb601e320bbc063ca608314635b7aabf
Kacper Klassa <kacper.klassa@hyland.com>
2022-09-15 08:54:18 +0200

    move '|| true' inside brackets

--- .github/actions/dbp-charts/verify-compose/docker_compose.sh
+++ .github/actions/dbp-charts/verify-compose/docker_compose.sh
@@ -33,12 +33,12 @@ COUNTER=0
 TIMEOUT=300
 t0=$(date +%s)
 echo "Waiting for alfresco to start"
-response=$(curl --write-out %{http_code} --output /dev/null --silent http://localhost:"${alf_port}"/alfresco/) || true
+response=$(curl --write-out %{http_code} --output /dev/null --silent http://localhost:"${alf_port}"/alfresco/ || true)
 until [[ "200" -eq "${response}" ]] || [[ "${COUNTER}" -eq "${TIMEOUT}" ]]; do
   printf '.'
   sleep "${WAIT_INTERVAL}"
   COUNTER=$((COUNTER + WAIT_INTERVAL))
-  response=$(curl --write-out %{http_code} --output /dev/null --silent http://localhost:"${alf_port}"/alfresco/) || true
+  response=$(curl --write-out %{http_code} --output /dev/null --silent http://localhost:"${alf_port}"/alfresco/ || true)
 done
 if (("${COUNTER}" < "${TIMEOUT}")); then
   t1=$(date +%s)
@@ -52,12 +52,12 @@ else
 fi
 COUNTER=0
 echo "Waiting for share to start"
-response=$(curl --write-out %{http_code} --output /dev/null --silent http://localhost:8080/share/page) || true
+response=$(curl --write-out %{http_code} --output /dev/null --silent http://localhost:8080/share/page || true)
 until [[ "200" -eq "${response}" ]] || [[ "${COUNTER}" -eq "${TIMEOUT}" ]]; do
   printf '.'
   sleep "${WAIT_INTERVAL}"
   COUNTER=$((COUNTER + WAIT_INTERVAL))
-  response=$(curl --write-out %{http_code} --output /dev/null --silent http://localhost:8080/share/page) || true
+  response=$(curl --write-out %{http_code} --output /dev/null --silent http://localhost:8080/share/page || true)
 done
 if (("${COUNTER}" < "${TIMEOUT}")); then
   t1=$(date +%s)
@@ -72,12 +72,12 @@ fi
 COUNTER=0
 TIMEOUT=20
 echo "Waiting more time for SOLR"
-response=$(curl --write-out %{http_code} --user admin:admin --output /dev/null --silent http://localhost:"${alf_port}"/alfresco/s/api/solrstats) || true
+response=$(curl --write-out %{http_code} --user admin:admin --output /dev/null --silent http://localhost:"${alf_port}"/alfresco/s/api/solrstats || true)
 until [[ "200" -eq "${response}" ]] || [[ "${COUNTER}" -eq "${TIMEOUT}" ]]; do
   printf '.'
   sleep "${WAIT_INTERVAL}"
   COUNTER=$((COUNTER + WAIT_INTERVAL))
-  response=$(curl --write-out %{http_code} --user admin:admin --output /dev/null --silent http://localhost:"${alf_port}"/alfresco/s/api/solrstats) || true
+  response=$(curl --write-out %{http_code} --user admin:admin --output /dev/null --silent http://localhost:"${alf_port}"/alfresco/s/api/solrstats || true)
 done
 
 cd ..
