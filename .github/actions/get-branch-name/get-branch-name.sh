3f7de197c7933a89b5ce263c00318ebeaed7b060
Giovanni Toraldo <giovanni.toraldo@hyland.com>
2022-10-18 11:12:46 +0200

    add bats support

--- .github/actions/get-branch-name/get-branch-name.sh
+++ .github/actions/get-branch-name/get-branch-name.sh
@@ -1,7 +1,12 @@
 #!/bin/bash -e
 BRANCH_NAME=${GITHUB_HEAD_REF:-$GITHUB_REF_NAME}
 if [ "$SANITIZE" = "true" ]; then
-    BRANCH_NAME=$(echo "$BRANCH_NAME"| tr -d "." | tr "/" "-" | tr "[:upper:]" "[:lower:]")
+    BRANCH_NAME=$(echo "$BRANCH_NAME" | tr -d "." | tr "/" "-" | tr "[:upper:]" "[:lower:]")
 fi
+if [ "$MAX_LENGTH" -gt "0" ]; then
+    BRANCH_NAME="${BRANCH_NAME:0:$MAX_LENGTH}"
+    BRANCH_NAME=$(echo "$BRANCH_NAME" | sed 's/-$//')
+fi
+
 echo "Branch name is '$BRANCH_NAME'"
 echo "BRANCH_NAME=$BRANCH_NAME" >> $GITHUB_ENV
