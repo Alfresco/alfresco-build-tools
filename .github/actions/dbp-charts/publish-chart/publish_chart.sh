88063ae20fa0f16ff559f34b8440592eb4c67e7e
Giovanni Toraldo <giovanni.toraldo@hyland.com>
2022-11-16 18:02:43 +0100

    OPSEXP-1796 Simplify publish-chart release skipping tags (#180)

--- .github/actions/dbp-charts/publish-chart/publish_chart.sh
+++ .github/actions/dbp-charts/publish-chart/publish_chart.sh
@@ -1,40 +1,26 @@
 #!/bin/bash -e
-
 HELM_REPO_BASE_URL="https://kubernetes-charts.alfresco.com"
 CHART_VERSION=$(yq eval .version helm/"${PROJECT_NAME}"/Chart.yaml)
 
-if [[ "${RELEASE_TYPE}" == "stable" ]]; then
-  export HELM_REPO=stable
-  git checkout -B "${BRANCH_NAME}"
-  git config --global user.name "${GITHUB_USERNAME}"
-  git config --global user.email "${GITHUB_EMAIL}"
-  echo "Tagging repository with v${CHART_VERSION}..."
-  export GIT_TAG="v${CHART_VERSION}"
-  git tag "${GIT_TAG}" -a -m "Generated tag from github action for build ${GITHUB_RUN_NUMBER}"
-  git push https://"${GITHUB_TOKEN}"@github.com/${PROJECT_REPO} "${GIT_TAG}"
-  git tag -d latest || true
-  git tag -a -m "current latest -> ${GIT_TAG}" -f latest "${GIT_TAG}"^{}
-  for ref in ':refs/tags/latest' 'latest'; do
-    git push https://"${GITHUB_TOKEN}"@github.com/${PROJECT_REPO} "${ref}"
-  done
-else
+if [[ "$CHART_VERSION" == *"$ALPHA_SUFFIX"* ]]; then
   export HELM_REPO=incubator
-  ALPHA_BUILD_VERSION="${CHART_VERSION%-*}-${ALPHA_SUFFIX}"
-  echo "Changing Chart version to ${ALPHA_BUILD_VERSION} as this is a feature branch..."
-  sed -i s,"${CHART_VERSION}","${ALPHA_BUILD_VERSION}",g helm/"${PROJECT_NAME}"/Chart.yaml
+else
+  export HELM_REPO=stable
 fi
 
-COMMIT_MESSAGE_FIRST_LINE=$(git log --pretty=format:%s --max-count=1)
-echo using COMMIT_MESSAGE_FIRST_LINE="${COMMIT_MESSAGE_FIRST_LINE}"
-git config --global user.email "alfresco-build@alfresco.com"
-git config --global user.name "alfresco-build"
-git clone https://"${GITHUB_TOKEN}"@github.com/Alfresco/charts.git
-echo using PROJECT_NAME="${PROJECT_NAME}",BRANCH="${BRANCH_NAME}",HELM_REPO="${HELM_REPO}"
+COMMIT_MESSAGE="Publishing ${PROJECT_NAME} v${CHART_VERSION} on ${HELM_REPO} repo"
+echo "$COMMIT_MESSAGE"
+echo '---'
+
+git config --global user.name "${GH_USERNAME}"
+git config --global user.email "${GH_EMAIL}"
+git clone https://"${GH_TOKEN}"@github.com/Alfresco/charts.git
+echo using PROJECT_NAME="${PROJECT_NAME}",HELM_REPO="${HELM_REPO}"
 mkdir repo
 helm package --dependency-update --destination repo helm/"${PROJECT_NAME}"
 helm repo index repo --url "${HELM_REPO_BASE_URL}"/"${HELM_REPO}" --merge charts/"${HELM_REPO}"/index.yaml
 mv repo/* charts/"${HELM_REPO}"
 cd charts
 git add "${HELM_REPO}"
-git commit -m "${COMMIT_MESSAGE_FIRST_LINE}"
+git commit -m "$COMMIT_MESSAGE"
 git push --quiet origin master
