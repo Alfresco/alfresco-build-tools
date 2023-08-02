23d85cd9546b1040ee9756655cfa1cb3b9cdb623
mstrankowski <marcin.strankowski@hyland.com>
2023-08-02 21:31:16 +0200

    Unify approach to setting variable value

--- .github/actions/veracode/source_clear.sh
+++ .github/actions/veracode/source_clear.sh
@@ -9,7 +9,7 @@ mvn -B -q clean install \
     -Dmaven.javadoc.skip=true \
     com.srcclr:srcclr-maven-plugin:scan \
     -Dcom.srcclr.apiToken=${SRCCLR_API_TOKEN} > scan.log \
-    $ADDITIONAL_SETTINGS
+    ${ADDITIONAL_SETTINGS}
 
 SUCCESS=$?   # this will read exit code of the previous command
 
