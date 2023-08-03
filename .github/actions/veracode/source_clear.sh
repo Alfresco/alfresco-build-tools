e6577ab9d0b922f23cad8b717075fea3f6c84f27
mstrankowski <marcin.strankowski@hyland.com>
2023-08-03 11:31:55 +0200

    Fixes after PR review

--- .github/actions/veracode/source_clear.sh
+++ .github/actions/veracode/source_clear.sh
@@ -5,11 +5,9 @@ PS4="\[\e[35m\]+ \[\e[m\]"
 set +e -v -x
 
 mvn -B -q clean install \
-    -DskipTests \
-    -Dmaven.javadoc.skip=true \
+    ${SRCCLR_INSTALL_DEFAULT_OPTIONS} ${SRCCLR_INSTALL_OPTIONS} \
     com.srcclr:srcclr-maven-plugin:scan \
-    -Dcom.srcclr.apiToken=${SRCCLR_API_TOKEN} > scan.log \
-    ${ADDITIONAL_SETTINGS}
+    -Dcom.srcclr.apiToken=${SRCCLR_API_TOKEN} > scan.log
 
 SUCCESS=$?   # this will read exit code of the previous command
 
