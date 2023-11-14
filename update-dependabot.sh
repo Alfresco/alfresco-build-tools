e070c2309e7c5ce65c8207c24d2d6f5549784167
Giovanni Toraldo <giovanni.toraldo@hyland.com>
2023-11-14 09:51:43 +0100

    Precommit support for update dependabot

--- update-dependabot.sh
+++ update-dependabot.sh
@@ -3,7 +3,10 @@
 # Directory containing composite actions
 actions_dir='.github/actions'
 
-# Dependabot config file
+# Dependabot source config file
+master_config='.github/dependabot.template.yml'
+
+# Dependabot target config file
 dependabot_config='.github/dependabot.yml'
 
 # Temp file for new Dependabot config section
@@ -12,7 +15,6 @@ temp_config='temp_dependabot.yml'
 # Function to generate Dependabot config section for a composite action
 generate_dependabot_section() {
     local action_dir=$1
-    local action_name=$(basename "$action_dir")
     echo "  - package-ecosystem: \"github-actions\""
     echo "    directory: \"/${action_dir}\""
     echo "    schedule:"
@@ -20,8 +22,8 @@ generate_dependabot_section() {
 }
 
 # Check if Dependabot config exists
-if [ ! -f "$dependabot_config" ]; then
-    echo "Dependabot config not found."
+if [ ! -f "$master_config" ]; then
+    echo "$master_config config not found."
     exit 1
 fi
 
@@ -30,12 +32,12 @@ echo "version: 2" > "$temp_config"
 echo "updates:" >> "$temp_config"
 
 # Loop through composite actions and append to temp config
-for action_dir in $(find "$actions_dir" -mindepth 1 -maxdepth 1 -type d -printf '%h\0%d\0%p\n' | sort -t '\0' -n | awk -F '\0' '{print $3}'); do
+for action_dir in $(find "$actions_dir" -mindepth 1 -maxdepth 1 -type d | sort -n); do
     generate_dependabot_section "$action_dir" >> "$temp_config"
 done
 
 # Merge new section with existing Dependabot config using yq
-yq -i "$dependabot_config" "$temp_config"
+yq eval-all '. as $item ireduce ({}; . *+ $item)' $master_config $temp_config > "$dependabot_config"
 
 # Clean up
 rm "$temp_config"
