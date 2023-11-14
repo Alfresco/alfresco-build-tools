6d20cc2b93e4ef81d5fabf55e94bc42986b38df0
Anahide Tchertchian <608958+atchertchian@users.noreply.github.com>
2023-11-14 10:52:59 +0100

    AAE-18008: Make dependabot update actions (#344)
    Co-authored-by: Giovanni Toraldo <giovanni.toraldo@hyland.com>

---
+++ update-dependabot.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Directory containing composite actions
+actions_dir='.github/actions'
+
+# Dependabot source config file
+master_config='.github/dependabot.template.yml'
+
+# Dependabot target config file
+dependabot_config='.github/dependabot.yml'
+
+# Temp file for new Dependabot config section
+temp_config='temp_dependabot.yml'
+
+# Function to generate Dependabot config section for a composite action
+generate_dependabot_section() {
+    local action_dir=$1
+    echo "  - package-ecosystem: \"github-actions\""
+    echo "    directory: \"/${action_dir}\""
+    echo "    schedule:"
+    echo "      interval: \"weekly\""
+}
+
+# Check if Dependabot config exists
+if [ ! -f "$master_config" ]; then
+    echo "$master_config config not found."
+    exit 1
+fi
+
+# Start building the new config section
+echo "version: 2" > "$temp_config"
+echo "updates:" >> "$temp_config"
+
+# Loop through composite actions and append to temp config
+for action_dir in $(find "$actions_dir" -mindepth 1 -maxdepth 1 -type d | sort -n); do
+    generate_dependabot_section "$action_dir" >> "$temp_config"
+done
+
+# Merge new section with existing Dependabot config using yq
+yq eval-all '. as $item ireduce ({}; . *+ $item)' $master_config $temp_config > "$dependabot_config"
+
+# Clean up
+rm "$temp_config"
