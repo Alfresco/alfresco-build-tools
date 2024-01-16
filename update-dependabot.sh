e990f8c8671af26d6a736f261d2d7d55fdf9825d
Giovanni Toraldo <giovanni.toraldo@hyland.com>
2024-01-16 09:39:10 +0100

    Reduce dependabot PRs for our actions by grouping updates

--- update-dependabot.sh
+++ update-dependabot.sh
@@ -15,10 +15,16 @@ temp_config='temp_dependabot.yml'
 # Function to generate Dependabot config section for a composite action
 generate_dependabot_section() {
     local action_dir=$1
-    echo "  - package-ecosystem: \"github-actions\""
-    echo "    directory: \"/${action_dir}\""
-    echo "    schedule:"
-    echo "      interval: \"weekly\""
+    cat <<END
+- package-ecosystem: "github-actions"
+  directory: "/${action_dir}"
+  schedule:
+    interval: "weekly"
+  groups:
+    catch-all:
+      patterns:
+        - "*"
+END
 }
 
 # Check if Dependabot config exists
@@ -28,17 +34,17 @@ if [ ! -f "$master_config" ]; then
 fi
 
 # Start building the new config section
-echo "version: 2" > "$temp_config"
-echo "updates:" >> "$temp_config"
+echo "version: 2" >"$temp_config"
+echo "updates:" >>"$temp_config"
 
 # Loop through composite actions and append to temp config
 for action_filename in $(find "$actions_dir" -mindepth 2 -type f -name action.yml | env -i LC_COLLATE=C sort -n); do
     action_dir=$(dirname $action_filename)
-    generate_dependabot_section "$action_dir" >> "$temp_config"
+    generate_dependabot_section "$action_dir" >>"$temp_config"
 done
 
 # Merge new section with existing Dependabot config using yq
-yq eval-all '. as $item ireduce ({}; . *+ $item)' $master_config $temp_config > "$dependabot_config"
+yq eval-all '. as $item ireduce ({}; . *+ $item)' $master_config $temp_config >"$dependabot_config"
 
 # Clean up
 rm "$temp_config"
