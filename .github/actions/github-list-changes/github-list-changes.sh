5991a7ebb573f991b5a087fe4d94b7577cb9adb1
Tom Page <tpage-alfresco@users.noreply.github.com>
2024-07-12 11:34:30 +0100

    ACS-8392 Feature/GitHub changed files (#687)
    Co-authored-by: Giovanni Toraldo <71768+gionn@users.noreply.github.com>

---
+++ .github/actions/github-list-changes/github-list-changes.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+
+if [[ $GITHUB_EVENT_NAME == "pull_request" ]]; then
+    # Get the list of changed files from the pull request.
+    git diff --name-only origin/$GITHUB_BASE_REF refs/remotes/pull/$PULL_REQUEST_NUMBER/merge > all-changed-files.txt
+elif [[ $GITHUB_EVENT_NAME == "push" ]]; then
+    # Check if the old commit exists (it might not for force pushes).
+    old_commit=$BEFORE_COMMIT
+    # If it doesn't exist, then run against the (single) latest commit.
+    git log -1 $old_commit > /dev/null 2>&1 || old_commit="$AFTER_COMMIT~"
+    # Get the list of changed files from the pushed commits.
+    git diff --name-only $old_commit $AFTER_COMMIT > all-changed-files.txt
+else
+    echo "Unsupported event type: $GITHUB_EVENT_NAME"
+    exit 1
+fi
+
+echo "List of changed files:"
+cat all-changed-files.txt
+
+# Multiline output requires a unique delimiter.
+EOF="EOF_9f507bc5-395d-47a6-83c7-ef23e9f5d313"
+
+# Write the list to the GitHub step output.
+echo "all-changed-files<<$EOF" >> $GITHUB_OUTPUT
+echo "$( cat all-changed-files.txt )" >> $GITHUB_OUTPUT
+echo "$EOF" >> $GITHUB_OUTPUT
+
+# Write the list to the environment variable if requested.
+if [[ "$WRITE_LIST_TO_ENV" == "true" ]]; then
+    echo "GITHUB_MODIFIED_FILES<<$EOF" >> $GITHUB_ENV
+    echo "$( cat all-changed-files.txt )" >> $GITHUB_ENV
+    echo "$EOF" >> $GITHUB_ENV
+fi
