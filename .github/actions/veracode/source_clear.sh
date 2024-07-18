56658c86a1af8a4c945889b7925d307640e83e81
Marcin Strankowski <marcin.strankowski@hyland.com>
2024-07-18 21:14:47 +0200

    ACSA-252: Switch SCA scan from maven to srcclr
    Still needs some work when we get information about setting maven options to skip test modules in scans. Waiting on veracode feedback on this

--- .github/actions/veracode/source_clear.sh
+++ .github/actions/veracode/source_clear.sh
@@ -4,10 +4,7 @@ echo "=========================== Starting SourceClear Script ==================
 PS4="\[\e[35m\]+ \[\e[m\]"
 set +e -v -x
 
-mvn -B -q clean install \
-    ${SRCCLR_INSTALL_DEFAULT_OPTIONS} ${SRCCLR_INSTALL_OPTIONS} \
-    com.srcclr:srcclr-maven-plugin:scan \
-    -Dcom.srcclr.apiToken=${SRCCLR_API_TOKEN} > scan.log
+srcclr scan > scan.log
 
 SUCCESS=$?   # this will read exit code of the previous command
 
