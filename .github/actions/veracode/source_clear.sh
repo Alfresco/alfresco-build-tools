f737edb81cdaa219540ec2cd8de88af1ed36b726
Marcin Strankowski <marcin.strankowski@hyland.com>
2024-07-26 18:53:11 +0200

    Post-testing changes
    Works for both regular pushes and PRs, tested on testing branch stemming from alfresco-enterprise-repo/7.4.N

--- .github/actions/veracode/source_clear.sh
+++ .github/actions/veracode/source_clear.sh
@@ -4,7 +4,11 @@ echo "=========================== Starting SourceClear Script ==================
 PS4="\[\e[35m\]+ \[\e[m\]"
 set +e -v -x
 
-srcclr scan > scan.log
+srcclr scan \
+  --scm-uri="$SRCCLR_SCM_URI" \
+  --scm-ref="$SRCCLR_SCM_REF" \
+  --scm-ref-type="$SRCCLR_SCM_REF_TYPE" \
+  --scm-rev="$SRCCLR_SCM_REV" > scan.log
 
 SUCCESS=$?   # this will read exit code of the previous command
 
