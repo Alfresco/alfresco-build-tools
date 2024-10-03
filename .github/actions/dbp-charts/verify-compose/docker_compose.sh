30177f826ee710913d9bda0b4302b5cad4e6fd69
Alex Chapellon <alexandre.Chapellon@hyland.com>
2024-10-03 17:13:55 +0200

    OPSEXP-2801: refactor and bugfix of dbp-charts/verify-compose action (#761)

--- .github/actions/dbp-charts/verify-compose/docker_compose.sh
+++ .github/actions/dbp-charts/verify-compose/docker_compose.sh
@@ -5,6 +5,26 @@ COMPOSE_PATH=$(dirname $COMPOSE_FILE_PATH)
 COMPOSE_BIN="docker compose"
 alf_port=8080
 
+dump_all_compose_logs() {
+  echo "Dumping logs for all containers"
+  $COMPOSE_BIN -f "${COMPOSE_FILE}" logs --no-color
+  exit 1
+}
+
+wait_result() {
+  COMPONENT=$1
+  if (("${COUNTER}" < "${TIMEOUT}")); then
+    t1=$(date +%s)
+    delta=$(((t1 - t0) / 60))
+    echo "$COMPONENT Started in ${delta} minutes"
+  else
+    echo "Waited ${COUNTER} seconds"
+    echo "$COMPONENT could not start in time."
+    echo "The last response code was ${response}"
+    dump_all_compose_logs
+  fi
+}
+
 cd "$COMPOSE_PATH" || {
   echo "Error: docker compose dir not found"
   exit 1
@@ -13,7 +33,7 @@ docker info
 $COMPOSE_BIN version
 $COMPOSE_BIN -f "${COMPOSE_FILE}" config
 echo "Starting Alfresco in docker compose"
-$COMPOSE_BIN ps
+$COMPOSE_BIN -f "${COMPOSE_FILE}" ps
 if [ "$COMPOSE_PULL" = "true" ]; then
   $COMPOSE_BIN -f "${COMPOSE_FILE}" pull --quiet
 fi
@@ -32,16 +52,7 @@ until [[ "200" -eq "${response}" ]] || [[ "${COUNTER}" -eq "${TIMEOUT}" ]]; do
   COUNTER=$((COUNTER + WAIT_INTERVAL))
   response=$(curl --write-out '%{http_code}' --output /dev/null --silent http://localhost:"${alf_port}"/alfresco/ || true)
 done
-if (("${COUNTER}" < "${TIMEOUT}")); then
-  t1=$(date +%s)
-  delta=$(((t1 - t0) / 60))
-  echo "Alfresco Started in ${delta} minutes"
-else
-  echo "Waited ${COUNTER} seconds"
-  echo "Alfresco could not start in time."
-  echo "The last response code from /alfresco/ was ${response}"
-  exit 1
-fi
+wait_result Alfresco
 
 COUNTER=0
 echo "Waiting for share to start"
@@ -52,33 +63,25 @@ until [[ "200" -eq "${response}" ]] || [[ "${COUNTER}" -eq "${TIMEOUT}" ]]; do
   COUNTER=$((COUNTER + WAIT_INTERVAL))
   response=$(curl --write-out '%{http_code}' --output /dev/null --silent http://localhost:8080/share/page || true)
 done
-if (("${COUNTER}" < "${TIMEOUT}")); then
-  t1=$(date +%s)
-  delta=$(((t1 - t0) / 60))
-  echo "Share Started in ${delta} minutes"
-else
-  echo "Waited ${COUNTER} seconds"
-  echo "Share could not start in time."
-  echo "The last response code from /share/ was ${response}"
-  exit 1
-fi
+wait_result Share
 
-COUNTER=0
-TIMEOUT=20
-echo "Waiting more time for SOLR"
-response=$(curl --write-out '%{http_code}' --user admin:admin --output /dev/null --silent http://localhost:"${alf_port}"/alfresco/s/api/solrstats || true)
-until [[ "200" -eq "${response}" ]] || [[ "${COUNTER}" -eq "${TIMEOUT}" ]]; do
-  printf '.'
-  sleep "${WAIT_INTERVAL}"
-  COUNTER=$((COUNTER + WAIT_INTERVAL))
+if [ $($COMPOSE_BIN -f "${COMPOSE_FILE}" config | yq '.services.alfresco.environment.JAVA_OPTS | contains(" -Dindex.subsystem.name=solr6")') == "true" ]; then echo "Waiting more time for SOLR"
+  COUNTER=0
+  TIMEOUT=20
+
   response=$(curl --write-out '%{http_code}' --user admin:admin --output /dev/null --silent http://localhost:"${alf_port}"/alfresco/s/api/solrstats || true)
-done
+  until [[ "200" -eq "${response}" ]] || [[ "${COUNTER}" -eq "${TIMEOUT}" ]]; do
+    printf '.'
+    sleep "${WAIT_INTERVAL}"
+    COUNTER=$((COUNTER + WAIT_INTERVAL))
+    response=$(curl --write-out '%{http_code}' --user admin:admin --output /dev/null --silent http://localhost:"${alf_port}"/alfresco/s/api/solrstats || true)
+  done
+  wait_result Solr
+fi
 
 cd ..
 docker run -a STDOUT --volume "${PWD}"/test/postman/docker-compose:/etc/newman --network host postman/newman:5.3 run "acs-test-docker-compose-collection.json" --global-var "protocol=http" --global-var "url=localhost:8080"
+
 retVal=$?
-if [ "${retVal}" -ne 0 ]; then
-  # show logs
-  $COMPOSE_BIN logs --no-color
-  exit 1
-fi
+
+[ "${retVal}" -eq 0 ] && echo "Postman tests were successful" || dump_all_compose_logs
