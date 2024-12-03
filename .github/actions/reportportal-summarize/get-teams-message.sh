95f9bbab4c03da957e53221f0a850cce86104859
Anahide Tchertchian <608958+atchertchian@users.noreply.github.com>
2024-12-03 20:23:42 +0100

    AAE-28530 Teams notifications improvements
    - move to adaptative cards for send-teams-notification-action: unused input "overwrite" was removed
    - add markdown message support for teams usage from reportportal-summarize

---
+++ .github/actions/reportportal-summarize/get-teams-message.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+
+if [[ -n "$RP_LAUNCH_KEY" ]]; then
+
+  NB=$(echo "$RP_CONTENT" | jq -r '.page.totalElements // "0"')
+  if [[ "$NB" == "0" || -z "$NB" ]]; then
+    MSG+="No report found for key "'`'"$RP_LAUNCH_KEY"'`'"."
+    MSG+="\n\nSee [latest reports]($RP_LAUNCH_URL)."
+  elif [ "$NB" == "1" ]; then
+    RP_LAUNCH_ID=$(echo "$RP_CONTENT" | jq -r '.content[0].id // empty')
+    STATUS=$(echo "$RP_CONTENT" | jq -r '.content[0].status // empty')
+    [[ "$STATUS" == 'PASSED' ]] && ICON="✅" || ICON="❌"
+    MSG+="See [report]($RP_LAUNCH_URL/$RP_LAUNCH_ID) $ICON"
+  else
+    MSG+="$NB reports found for key "'`'"$RP_LAUNCH_KEY"'`'"."
+    while read -r id ; do
+      read -r number
+      read -r status
+      case $status in
+        PASSED)
+        sstatus="✅" ;;
+        FAILED)
+        sstatus="❌"
+        ;;
+        *)
+        sstatus="$status"
+        ;;
+      esac
+      MSG+="\n\n[Report #$number]($RP_LAUNCH_URL/$id) $sstatus"
+    done < <(echo "$RP_CONTENT" | jq -r '.content[] | .id, .number, .status')
+  fi
+fi
+
+echo "message=$MSG" >> $GITHUB_OUTPUT
