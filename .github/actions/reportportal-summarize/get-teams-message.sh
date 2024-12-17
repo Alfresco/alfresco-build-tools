629ed2accaa1734d66e898c0d18d556328156d45
Anahide Tchertchian <608958+atchertchian@users.noreply.github.com>
2024-12-17 15:28:27 +0100

    AAE-28530 Fix ReportPortal launch key (#838)

--- .github/actions/reportportal-summarize/get-teams-message.sh
+++ .github/actions/reportportal-summarize/get-teams-message.sh
@@ -4,7 +4,7 @@ if [[ -n "$RP_LAUNCH_KEY" ]]; then
 
   NB=$(echo "$RP_CONTENT" | jq -r '.page.totalElements // "0"')
   if [[ "$NB" == "0" || -z "$NB" ]]; then
-    MSG+="No report found for key "'`'"$RP_LAUNCH_KEY"'`'"."
+    MSG+="No report found for key $RP_LAUNCH_KEY."
     MSG+="\n\nSee [latest reports]($RP_LAUNCH_URL)."
   elif [ "$NB" == "1" ]; then
     RP_LAUNCH_ID=$(echo "$RP_CONTENT" | jq -r '.content[0].id // empty')
@@ -12,7 +12,7 @@ if [[ -n "$RP_LAUNCH_KEY" ]]; then
     [[ "$STATUS" == 'PASSED' ]] && ICON="✅" || ICON="❌"
     MSG+="See [report]($RP_LAUNCH_URL/$RP_LAUNCH_ID) $ICON"
   else
-    MSG+="$NB reports found for key "'`'"$RP_LAUNCH_KEY"'`'"."
+    MSG+="$NB reports found for key $RP_LAUNCH_KEY."
     while read -r id ; do
       read -r number
       read -r status
