035e3c151f973e8e6717947bf530baa99ea3573a
Frantz FISCHER <frantz.fischer@hyland.com>
2026-02-12 16:41:43 +0100

    add params and check

--- .github/actions/jira-get-or-create-release/action.py
+++ .github/actions/jira-get-or-create-release/action.py
@@ -1,1 +1,18 @@
-print("HELLO !")
+import os
+import sys
+
+
+def get_required_env(var_name: str) -> str:
+  value = os.getenv(var_name)
+
+  if value is None or value.strip() == "":
+    print(f"❌ Missing required environment variable: {var_name}", file=sys.stderr)
+    sys.exit(1)
+
+  return value
+
+
+jira_user = get_required_env("JIRA_USER")
+jira_token = get_required_env("JIRA_TOKEN")
+
+print(f"{jira_user = }")
