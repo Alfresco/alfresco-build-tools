645c89d7c725fced4bcb48cadafab3963caa2b4f
Frantz FISCHER <frantz.fischer@hyland.com>
2026-02-19 09:50:52 +0100

    disable integration tests

--- .github/tests/python-scripts/jira-get-or-create-release/integration/test_action.py
+++ .github/tests/python-scripts/jira-get-or-create-release/integration/test_action.py
@@ -107,6 +107,7 @@ def created_version_ids(jira_env):
             pass
 
 
+@pytest.mark.skip(reason="Need ACR-5754 to be resolved")
 def test_create_without_description(
     jira_env,
     unique_version_name,
@@ -141,6 +142,7 @@ def test_create_without_description(
     assert (version.get("description") or "").strip() == ""
 
 
+@pytest.mark.skip(reason="Need ACR-5754 to be resolved")
 def test_create_with_description(
     jira_env,
     unique_version_name,
@@ -176,6 +178,7 @@ def test_create_with_description(
     assert (version.get("description") or "").strip() == description
 
 
+@pytest.mark.skip(reason="Need ACR-5754 to be resolved")
 def test_create_twice_returns_same_version_id(
     jira_env,
     unique_version_name,
